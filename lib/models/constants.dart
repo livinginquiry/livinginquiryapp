1becd0d78e7a88565767e1e2d8b56c4ec4545840
Zac Wolfe <zac.wolfe@gmail.com>
2021-03-11 22:46:57 -0800

    Update the color options for worksheets
    https://app.asana.com/0/1152783500105769/1199922339216948

--- lib/models/constants.dart
+++ lib/models/constants.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 const NOTE_COLORS = [
-  Color(0xffffffff), // classic white
-  Color(0xfff28b81), // light pink
-  Color(0xfff7bd02), // yellow
-  Color(0xfffbf476), // light yellow
-  Color(0xffcdff90), // light green
-  Color(0xffa7feeb), // turquoise
-  Color(0xffcbf0f8), // light cyan
+  Color(0xffeef0f1), // grey
+  Color(0xffffdbdb), // red
+  Color(0xffffe5db), // orange
+  Color(0xffFFF9DB), // yellow
+  Color(0xffD1E5D0), // green
+  Color(0xffD0DBE5), // blue
+  Color(0xffE5DBE8), // purple
   Color(0xffafcbfa), // light blue
   Color(0xffd7aefc), // plum
   Color(0xfffbcfe9), // misty rose
